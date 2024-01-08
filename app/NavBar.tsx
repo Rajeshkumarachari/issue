02e5795a6a96a0c819e4ad9d119b87faa50e2507
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-01-08 09:57:52 +0530

    setup prisma

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Link from "next/link";
-import React from "react";
-import { GoIssueClosed } from "react-icons/go";
+import React, { use } from "react";
+import { BsBrilliance } from "react-icons/bs";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     {
       label: "Dashboard",
@@ -16,13 +21,17 @@ const NavBar = () => {
   return (
     <nav className=" flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
-        <GoIssueClosed />
+        <BsBrilliance />
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
           <Link
             key={link.href}
-            className=" text-zinc-500  hover:text-zinc-800 transition-colors"
+            className={classnames({
+              "text-zinc-900 underline": link.href == currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors ": true,
+            })}
             href={link.href}
           >
             {link.label}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Rajesh</div>;
+  return <div>Rajesh Dashboard</div>;
 }
